09b1941146c344413d7029541db8ad0658db2d2b
younusFoysal <younus.faysal@gmail.com>
2025-01-20 10:15:20 +0600

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 
-
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://nestUser:AK3dyHk1La0SBDG2@cluster0.mquum9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot(
+      'mongodb+srv://nestUser:AK3dyHk1La0SBDG2@cluster0.mquum9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
     TasksModule,
   ],
 })
